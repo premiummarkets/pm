0b80af1cdc2b4aaf950e66294052a646f9534d57
Gheeyom Thor <piggymarketsqueak@gmail.com>
2018-06-18 00:53:31 +0100

    Iterative operation

---
+++ premiumMarkets/pm-core/src/main/java/com/finance/pms/events/operations/nativeops/EventInfoIterativeOperation.java
@@ -0,0 +1,10 @@
+package com.finance.pms.events.operations.nativeops;
+
+public class EventInfoIterativeOperation extends PMDataFreeOperation {
+	
+	public EventInfoIterativeOperation() {
+		super("eventInfoIterative", "Incremental calculation of the same indicator within a date rage");
+		//TODO
+	}
+
+}
