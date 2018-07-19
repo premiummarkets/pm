d0776065b4511c8c42d1c267fd5d0793f6298e54
guillaume.thoreton <guillaume.thoreton@sage.com>
2018-07-19 11:17:20 +0100

    Adding dynamic column reference management for DoubleArrayMapValue

--- premiumMarkets/pm-core/src/main/java/com/finance/pms/events/operations/nativeops/ArrayMapOperation.java
+++ premiumMarkets/pm-core/src/main/java/com/finance/pms/events/operations/nativeops/ArrayMapOperation.java
@@ -1,9 +1,5 @@
 package com.finance.pms.events.operations.nativeops;
 
-import java.util.List;
-
 public interface ArrayMapOperation extends MapOperation {
-
-	List<String> columnsReferences();
 
 }
--- premiumMarkets/pm-core/src/main/java/com/finance/pms/events/operations/nativeops/DoubleArrayMapValue.java
+++ premiumMarkets/pm-core/src/main/java/com/finance/pms/events/operations/nativeops/DoubleArrayMapValue.java
@@ -1,8 +1,6 @@
 package com.finance.pms.events.operations.nativeops;
 
-import java.util.Date;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.*;
 
 import com.finance.pms.admin.install.logging.MyLogger;
 import com.finance.pms.events.operations.TargetStockInfo;
@@ -14,20 +12,29 @@ public class DoubleArrayMapValue extends Value<java.util.SortedMap<Date, double[
 	protected static MyLogger LOGGER = MyLogger.getLogger(DoubleArrayMapValue.class);
 
 	private SortedMap<Date, double[]> map;
+	private List<String> columnsReferences;
 
-	public DoubleArrayMapValue(SortedMap<Date, double[]> map) {
+	public DoubleArrayMapValue(SortedMap<Date, double[]> map, List<String> columnsReferences) {
 		super();
 		this.map = map;
+		this.columnsReferences = columnsReferences;
 	}
 
 	public DoubleArrayMapValue() {
 		super();
-		this.map = new TreeMap<Date, double[]>();
+		this.map = new TreeMap<>();
+		this.columnsReferences = new ArrayList<>();
 	}
 
 	@Override
 	public SortedMap<Date, double[]> getValue(TargetStockInfo targetStock) {
 		return map;
+	}
+
+	//The particularity of DoubleArrayMaps is that the column amount and refs is know only at runtime.
+	//Hence, this has to be held in the value not the operation.
+	public List<String> getColumnsReferences() {
+		return this.columnsReferences;
 	}
 
 	@Override
