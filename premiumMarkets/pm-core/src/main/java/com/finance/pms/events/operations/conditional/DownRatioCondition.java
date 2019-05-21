80008da72be7045af70d61f97ee52639a7b1bbbf
Gheeyom Thor <piggymarketsqueak@gmail.com>
2019-05-21 22:28:05 +0100

    Epsilon min crossing review

--- premiumMarkets/pm-core/src/main/java/com/finance/pms/events/operations/conditional/DownRatioCondition.java
+++ premiumMarkets/pm-core/src/main/java/com/finance/pms/events/operations/conditional/DownRatioCondition.java
@@ -48,7 +48,7 @@ public class DownRatioCondition extends CrossConstantCondition implements UnaryC
 	public Boolean conditionCheck(@SuppressWarnings("unchecked") Comparable<Double> ... ops) {
 		Double prev = (Double) ops[0];
 		Double current = (Double) ops[1];
-		Double ratio = ((Double) ops[2])/100;
+		Double ratio = ((Double) ops[2])/100; // division by 100 when used a ratio.
 		//Double epsilonMinCrossing = (Double) ops[3]; //Not used
 		if (prev <= 0 || current < 0) throw new UnsupportedOperationException("Values must be positive. Unmet condition as " + prev + " > 0 && " + current + " >= 0");
 		return (current - prev)/prev < -ratio;
